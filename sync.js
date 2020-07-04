9cef562765077662033ec6526eccaf7a432db9f6
Isaac Bandim <ibandim@outlook.com>
2020-07-04 10:02:22 -0300

    Sync and Assync form's with Node

---
+++ sync.js
@@ -0,0 +1,21 @@
+// Call five .txt write "Hello Node.js" in sync form 
+const fs = require('fs');
+
+for (let i = 1; i <= 5; i++){
+    const file = `sync-txt${i}.txt`
+    const out = fs.writeFileSync(file,'Hello Node.js!')
+    console.log(out);
+}
+//================================================
+
+//And now, i write .txt with assync form! All in same time!
+
+const fs = require('fs');
+
+for (let i=1; i <= 5; i++){
+    const file = `async-txt${i}.txt`;
+    fs.writeFile(file,'Hello Node.js!', (err,out) =>{
+        console.log(out);
+    })
+}
+
